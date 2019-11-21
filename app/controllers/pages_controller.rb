edb86988e875967a02454b7f95dd5686a600bfb8
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-11-21 10:36:18 +0000

    created app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about_me
+  end
+
+  def services_provided
+  end
+
+  def common_questions
+  end
+
+  def rates_insurance
+  end
+
+  def appointment_request
+  end
+
+  def contact_me
+  end
+
+  def privacy_policy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "about_me", to: 'pages#about_me'
+  get "services_provided", to: 'pages#services_provided'
+
+  get "common_questions", to: 'pages#common_questions'
+
+  get 'rates_insurance', to: 'pages#rates_insurance'
+
+  get 'appointment_request', to: 'pages#appointment_request'
+
+  get 'contact_me', to: 'pages#contact_me'
+
+  get 'privacy_policy', to: 'pages#privacy_policy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
